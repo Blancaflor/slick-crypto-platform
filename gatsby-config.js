e6973b3663a726659603d018241e8d4699d7b1dd
Pix3L Click3r <blancaflorkevin@gmail.com>
2022-12-22 13:28:32 +0100

    Dev

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,32 +4,5 @@ module.exports = {
     description: '[DIGITAL] - Partake, Wield, Stake, Employ',
     keywords: 'SLICK Blockchain Cryptocurrency, for Stakers, for Blockchain development, for Blockchain DAPP selling NFTs, Crypto DAPP Staking, beeing Meme Mascotte of organisations.'
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/static/images`,
-      },
-    },
-    `gatsby-plugin-styled-components`,
-    "gatsby-plugin-sharp",
-    // REMARK plugin needed to extract the markdown files and parses
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site./ This path is relative to the root of the site.
-      }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+  plugins: ['gatsby-plugin-react-helmet'],
 }
