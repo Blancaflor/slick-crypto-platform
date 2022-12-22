9b8836087d2e6d013f9b912a7292f8f677208225
Pix3L Click3r <blancaflorkevin@gmail.com>
2022-12-22 13:49:57 +0100

    Dev

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,4 +5,11 @@ module.exports = {
     keywords: 'SLICK Blockchain Cryptocurrency, for Stakers, for Blockchain development, for Blockchain DAPP selling NFTs, Crypto DAPP Staking, beeing Meme Mascotte of organisations.'
   },
   plugins: ['gatsby-plugin-react-helmet'],
+  icons: [
+    {
+      "src": "favicon.png",
+      "sizes": "64x64 32x32 24x24 16x16",
+      "type": "image/x-icon"
+    }
+  ],
 }
